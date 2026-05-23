6a800cf15f17de4cf1cd50d09fc832ea6d9268aa
Angel Rios <angel.rios.dev@gmail.com>
2026-05-23 14:59:56 -0600

    feat(db): agregar parametros_conexion JSONB a Instancia_DBMS y actualizar script de inicializacion

--- bd/documentacion/03_init.sql
+++ bd/documentacion/03_init.sql
@@ -17,12 +17,12 @@ INSERT INTO Servidor (nombre_servidor, direccion_ip, es_legacy, id_nivel_critici
 
 -- 2. REGISTRO DE INSTANCIAS DBMS
 -- IDs de DBMS: 1:Postgres, 2:MySQL 5.7, 3:MySQL 8.0, 4:Oracle, 5:MongoDB
-INSERT INTO Instancia_DBMS (nombre_instancia, puerto, id_servidor, id_dbms, id_estado_instancia) VALUES 
-('PostgreSQL Instance', 5432, 1, 1, 1),
-('MySQL 5.7 Instance', 3306, 2, 2, 1),
-('MySQL 8.0 Instance', 3306, 3, 3, 1),
-('Oracle 21c XE', 1521, 4, 4, 1),
-('MongoDB Cluster 0', 27017, 5, 5, 1);
+INSERT INTO Instancia_DBMS (nombre_instancia, puerto, id_servidor, id_dbms, id_estado_instancia, parametros_conexion) VALUES 
+('PostgreSQL Instance', 5432, 1, 1, 1, '{"database": "sgir_db", "user": "sgir"}'::jsonb),
+('MySQL 5.7 Instance', 3306, 2, 2, 1, '{"database": "sgir_db_5", "user": "sgir"}'::jsonb),
+('MySQL 8.0 Instance', 3306, 3, 3, 1, '{"database": "sgir_db_8", "user": "sgir"}'::jsonb),
+('Oracle 21c XE', 1521, 4, 4, 1, '{"service_name": "XEPDB1", "user": "system"}'::jsonb),
+('MongoDB Cluster 0', 27017, 5, 5, 1, '{"database": "admin", "user": "sgir"}'::jsonb);
 
 -- 4. BASES DE DATOS INICIALES
 INSERT INTO Base_de_Datos (nombre_base, id_instancia, id_estado_bd) VALUES 
